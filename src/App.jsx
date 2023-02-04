14607f05d81b7f74b4d482a8046de57b389a3860
Hamry <henry.lue@gmail.com>
2023-02-04 14:47:11 -0500

    Added search bar though it is still invis

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Destinations from './components/Destinations';
+import Search from './components/Search';
+import Selections from './components/Selections';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
+import SearchBar from './backend/Searchbar'
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <SearchBar/>
+      <Destinations />
+      <Search />
+      <Selections />
+      <Carousel />
+      <Footer />
+
+    </>
+  );
+};
+
+export default App;
