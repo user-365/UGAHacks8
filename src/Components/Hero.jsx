2006d11d8a8ba9f739517b5d22ce7f9cbf21cd79
Rian Rahman <rian.rahman@gatech.edu>
2023-02-05 02:04:56 -0500

    Refactored some code

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '/src/index.scss';
+import FAQs from '../Home/FAQs';
 
 const Hero = (props) => {
   return (
@@ -15,6 +16,7 @@ const Hero = (props) => {
       <div className='absolute top-0 left-0 w-full h-full flex flex-col justify-center text-center'>
         <h1 className='text-white mb-2'>{props.primaryDesc}</h1>
         <h2 className='text-white mb-4'>{props.secondaryDesc}</h2>
+        <FAQs/>  
       </div>
     </header>
   );
