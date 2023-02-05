f1a167c599f8982a388cff17120f3f125a483ef2
HardikJhunjhunwala <hardikjhunjhunwala@gmail.com>
2023-02-04 20:01:53 -0500

    Deleted

--- src/data/connections.jsx
+++
@@ -1,7 +0,0 @@
-const MongoClient = require("mongodb").MongoClient;
-const uri = "mongodb+srv://eunjung77819@gmail.com:gp12341234@cluster.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const db = client.db("test");
-  console.log("Connected to MongoDB");
-});
