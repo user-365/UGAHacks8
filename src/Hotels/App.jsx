3d8372e925e28d31f4a3f3ab1bc6b95f1b786ed5
Rian Rahman <rian.rahman@gatech.edu>
2023-02-04 20:22:09 -0500

    Made hotels look like before

--- src/Hotels/App.jsx
+++ src/Hotels/App.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 import Navbar from '../Components/Navbar';
 import Hero from '../Components/Hero';
 import Hotels from '../Hotels/Hotels';
+import Search from '../search/Search';
+import Selections from '../Components/Selections';
+import Carousel from '../Components/Carousel';
 import Footer from '../Components/Footer';
 import bgVideo from "../assets/hotelVid.mp4"
 
@@ -12,6 +15,8 @@ const App = () => {
       <Navbar />
       <Hero bgVideo={bgVideo} primaryDesc="Search Up Hotels" secondaryDesc="Minimizing Carbon Footprint"/>]
       <Hotels />
+      <Selections />
+      <Carousel />
       <Footer />
     </>
   );
