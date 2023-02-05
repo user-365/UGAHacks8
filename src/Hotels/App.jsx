6314b33be4a74a6f1b4ed6542d8e4a580fccbad4
Rian Rahman <rian.rahman@gatech.edu>
2023-02-05 02:51:41 -0500

    Fixed css error with search

--- src/Hotels/App.jsx
+++ src/Hotels/App.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 
 import Navbar from '../Components/Navbar';
 import Hero from '../Components/Hero';
+import Hotels from '../Hotels/Hotels';
+import Search from '../search/Search';
+import Selections from '../Components/Selections';
+import Carousel from '../Components/Carousel';
+import Footer from '../Components/Footer';
 import bgVideo from "../assets/hotelVid.mp4"
 
 const App = () => {
@@ -9,6 +14,7 @@ const App = () => {
     <>
       <Navbar />
       <Hero bgVideo={bgVideo} primaryDesc="Search Up Hotels" secondaryDesc="Minimizing Carbon Footprint" isHotel={true}/>
+      <Footer />
     </>
   );
 };
