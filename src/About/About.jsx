b6488ad6f4613c19cc58e9a7ef19740d949f3050
Rian Rahman <rian.rahman@gatech.edu>
2023-02-05 02:18:17 -0500

    Refactored about us page

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -6,12 +6,11 @@ import Hero from '../Components/Hero';
 const About = () => {
   const [ toggleTab, setToggleTab] = useState(1)
   const toggleState = (index) =>{setToggleTab(index)}
-  const description="A Carbon Footprint is the amount of Carbon Dioxide (CO2) released due to the consumption of fossil fuels by an individual or organization. Now imagine a world, where we could minimize Carbon Footprinting as much as possible when traveling. Our web app, EcoTrip, accomplishes this by suggesting sustainable travel options, such as green hotels and environmentally friendly modes of transportation."
-
+  
   return (
     <Fragment>
     <Navbar />
-    <Hero bgVideo={bgVideo} primaryDesc="About Us" paragraph={description}/>
+    <Hero bgVideo={bgVideo} primaryDesc="About Us" FAQs={true} />
     </Fragment>
   )
 }
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '/src/index.scss';
-import FAQs from '../Home/FAQs';
+import FAQs from '../About/FAQs';
 import Hotels from '../Hotels/Hotels';
 
 const Hero = (props) => {
@@ -17,8 +17,8 @@ const Hero = (props) => {
       <div className='absolute top-0 left-0 w-full h-full flex flex-col justify-center text-center'>
         <h1 className='text-white mb-2'>{props.primaryDesc}</h1>
         <h2 className='text-white mb-4'>{props.secondaryDesc}</h2>
-        <FAQs/>  
         <h4 className='text-white mb-4'>{props.paragraph}</h4>
+        {props.FAQs && <FAQs />}
         {props.isHotel && <Hotels />}
       </div>
     </header>
