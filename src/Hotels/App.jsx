3a3c0f8130e6043e9d86fc3ee37329ff1e010665
Rian Rahman <rian.rahman@gatech.edu>
2023-02-05 00:48:03 -0500

    More changes

--- src/Hotels/App.jsx
+++ src/Hotels/App.jsx
@@ -2,11 +2,6 @@ import React from 'react';
 
 import Navbar from '../Components/Navbar';
 import Hero from '../Components/Hero';
-import Hotels from '../Hotels/Hotels';
-import Search from '../search/Search';
-import Selections from '../Components/Selections';
-import Carousel from '../Components/Carousel';
-import Footer from '../Components/Footer';
 import bgVideo from "../assets/hotelVid.mp4"
 
 const App = () => {
